7e1f1f66f5ce9452ffa8a10ac324fc29bc6c9ca8
Sayan "Riju" Chakrabarti <s26c.sayan@gmail.com>
2017-10-20 22:53:25 +0530

    DEMO UI: Driver side

--- server.js
+++ server.js
@@ -14,20 +14,26 @@ app.use(bodyParser.json())
 app.use(Express.static(`${__dirname}/public`))
 
 const server = http.createServer(app)
-const bayeux = new Faye.NodeAdapter()
+const bayeux = new Faye.NodeAdapter({ mount: "/faye", timeout: 45 })
 bayeux.attach(server)
 
 bayeux.getClient()
   .subscribe("/location/*")
-  .withChannel((id, data) => {
+  .withChannel((channel, data) => {
+    const id = channel.split("/")[2]
+    console.log("FAYE: Rcvd location udpate msg........", id, data);
     if (id && data.lng && data.lat) {
       redis.geoadd("driver:locations", data.lng, data.lat, `id:${id}`)
+      // Also, publish a Heartbeat!
+      bayeux.getClient().publish(`/heartbeat/${id}`, { serverClient: true, timestamp: Date.now() })
     }
   })
 
 bayeux.getClient()
   .subscribe("/heartbeat/*")
-  .withChannel((id) => {
+  .withChannel((channel, data) => {
+    const id = channel.split("/")[2]
+    console.log("FAYE: Rcvd hearbeat msg......", id, data);
     if (id) {
       const timeOutInMinutes = 10 // time outs after minimum 10 mins of inactivity
       const timeOutStamp = addMinutes(Date.now(), timeOutInMinutes).valueOf()
@@ -41,7 +47,7 @@ app.post("/location", (req, res) => {
   if (!req.body.lng || !req.body.lat || !req.body.id) {
     return res.status(400).send("MISSING MANDATORY FIELDS IN REQUEST BODY")
   }
-  console.log(req.body.lng, req.body.lat, req.body.id);
+  console.log(req.body.lng, req.body.lat, req.body.id)
   redis.geoadd("driver:locations", req.body.lng, req.body.lat, `id:${req.body.id}`, (err) => {
     if (err) { return res.status(500).send("LOCATION NOT SAVED") }
     redis.zadd("driver:activeuntill", timeOutStamp, `id:${req.body.id}`) // no waiting!!
@@ -58,7 +64,7 @@ app.get("/near", (req, res) => {
 
   // First, find all inactive :
   redis.zrangebyscore("driver:activeuntill", "-inf", Date.now(), (err0, inactives) => {
-    console.log("Inactives: ", inactives);
+    console.log("Inactives: ", inactives)
     // Next, remove all those inactive  from geolocations
     redis.zrem("driver:locations", ...inactives, (err1) => {
       if (err1) {
